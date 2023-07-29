fcb92062740f290ba9f05e3d1a0710d373151ce7
Adarsh047 <adarshshanmugam2002@gmail.com>
2023-07-29 15:59:22 +0530

    completed all todos and merge with main branch

--- README.md
+++ README.md
@@ -1,11 +1,48 @@
 # Scraping-Google-Images
+
+## About
 This repo contains the python script to scrape from google images given any search keyword
 
 Under Construction
 
-TODO
+## TODO Timeline
 
 * Fix code and make the code work - July 23, 2023 ✅
 * Get the query from user input with any search keyword - July 25, 2023 ✅
-* Do not download duplicates - July 27, 2023 🚧
-* Store the downloaded images in specific Query folders - July 29, 2023 🚧
+* Do not download duplicates - July 27, 2023 ✅
+* Store the downloaded images in specific Query folders - July 29, 2023 ✅
+
+
+## Modules Used
+Modules and tools used in this project:
+
+* `bs4`
+* `selenium`
+* `webdriver_manager`
+
+## Project Installation
+Poetry is used for environment and module mangement in this project.
+Install poetry and set it as a path variable.
+
+For more details on how to install poetry visit this page:
+[Install and setup Poetry](https://python-poetry.org/docs/)
+
+To install all the requirements for this project, run the following commands:
+
+* `poetry shell` in the directory of the projecy to activate poetry
+* `poetry install` to install all the dependencies
+
+**Note**:All the dependencies required for this project is present inside the `poetry.lock` and `pyproject.toml` files.
+
+## Running the project
+
+To run the script type in the following in the terminal:
+* `poetry shell` in the directory of the projecy to activate poetry
+* `python scraping.py` in the terminal.
+
+### Parameters used in the script
+The script takes in three parameters:
+
+* Number of images
+* What images to download
+* Ready to download or not as a user input.This is done so that the user may scroll down in the window till the bottom of the page so that the dynamic loading of all images would occur.
--- scraping.py
+++ scraping.py
@@ -7,68 +7,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import os
 import time
+import random
+import string
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
+from pathlib import Path
 
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager(version="114.0.5735.90").install()))
-google_images = "https://www.google.com/imghp?hl=EN"
-driver.get(google_images)
+def generate_random_string(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
 
-n_img = input("Enter number of images you want")
-search = input("What images do you want?")
-text_area = driver.find_element(By.XPATH, '//textarea[@class="gLFyf"]')
-text_area.send_keys(search)
-button = driver.find_element(By.XPATH, '//button[@class="Tg7LZd"]')
-driver.execute_script("arguments[0].click();", button)
-a = input("Waiting for the user input to start..")
-
-
-page_html = driver.page_source
-pageSoup = bs4.BeautifulSoup(page_html, "html.parser")
-containers = pageSoup.findAll("div", {"class":"isv-r PNCib MSM1fd BUooTd"})
-
-len_containers = len(containers)
-print(f"Found {len_containers} images to download")
-
-img_urls = []
-
-def wait_for_elements_visibility(xpath):
-    wait = WebDriverWait(driver, 100)
-    elements = wait.until(EC.visibility_of_all_elements_located((By.XPATH, xpath)))
-    return elements
-
-# Define the XPath to locate all the images
-images_xpath = '//*[@class="wXeWr islib nfEiy"]'
-# Get all the image elements
-image_elements = wait_for_elements_visibility(images_xpath)
-# Loop through each image element and perform actions (click, extract information, etc.)
-try:
-    for i, image_element in enumerate(image_elements):
-        if i > int(n_img):
-            break
-        # For demonstration purposes, let's click on each image
-        image_element.click()
-        # You can perform any other action you want on each image element.
-        # Add your logic here...
-        opened_img_xpath = """//*[@id="Sva75c"]/div[2]/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]"""
-        max_wait = 0
-        while (driver.find_element(By.XPATH, opened_img_xpath).get_attribute("src").startswith("data:")
-            or driver.find_element(By.XPATH, opened_img_xpath).get_attribute("src").startswith("https://encrypted-tbn0")):
-            max_wait += 1
-            time.sleep(2)
-            if max_wait == 15:
-                break
-        opened_img_element = driver.find_element(By.XPATH, opened_img_xpath)
-        image_src_link = opened_img_element.get_attribute('src')
-        print(image_src_link)
-        img_urls.append(image_src_link)
-except Exception as e:
-    print("An error occurred:", e)
-
-
-finally:
-    driver.quit()
 
 def download_image(url, save_path):
     try:
@@ -77,27 +26,108 @@ def download_image(url, save_path):
 
         with open(save_path, 'wb') as f:
             f.write(response.content)
-        
+
         print(f"Image downloaded successfully: {url}")
 
     except requests.exceptions.RequestException as e:
         print(f"Error downloading image: {url}\n", e)
 
-with open('urls.txt','a+') as tfile:
-	tfile.write('\n'.join(img_urls))
 
-# Create a folder to save the downloaded images
-if not os.path.exists('images'):
-    os.makedirs('images')
+def main():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager(version="114.0.5735.90").install()))
+    google_images = "https://www.google.com/imghp?hl=EN"
+    driver.get(google_images)
 
-urls_open = open('urls.txt', 'r')
-Lines = urls_open.readlines()
-urls_open = [i.strip() for i in Lines]
+    n_img = input("Enter number of images you want:\n")
+    search = input("What images do you want?:\n")
+    text_area = driver.find_element(By.XPATH, '//textarea[@class="gLFyf"]')
+    text_area.send_keys(search)
+    root = Path("images")
+    save_folder = Path(search)
+    save_folder = root / save_folder
+    save_folder.mkdir(parents=True, exist_ok=True)
+    button = driver.find_element(By.XPATH, '//button[@class="Tg7LZd"]')
+    driver.execute_script("arguments[0].click();", button)
+    a = input("Waiting for the user input to start..")
 
-# Loop through the list of image URLs and download each image
-for idx, url in enumerate(img_urls, start=1):
-    # if url in urls_open:
-    #     continue
-    filename = f"image{idx}.jpg"  # You can customize the filenames if needed
-    save_path = os.path.join('images', filename)
-    download_image(url, save_path)
+    page_html = driver.page_source
+    pageSoup = bs4.BeautifulSoup(page_html, "html.parser")
+    containers = pageSoup.findAll("div", {"class": "isv-r PNCib MSM1fd BUooTd"})
+
+    len_containers = len(containers)
+    print(f"Found {len_containers} images to download")
+
+    img_urls = []
+
+    def wait_for_elements_visibility(xpath):
+        wait = WebDriverWait(driver, 20)
+        elements = wait.until(EC.visibility_of_all_elements_located((By.XPATH, xpath)))
+        return elements
+
+    # Define the XPath to locate all the images
+    images_xpath = '//*[@class="wXeWr islib nfEiy"]'
+    # Get all the image elements
+    image_elements = wait_for_elements_visibility(images_xpath)
+
+    # Loop through each image element and perform actions (click, extract information, etc.)
+    try:
+        for i, image_element in enumerate(image_elements):
+            if i >= int(n_img):
+                break
+            # For demonstration purposes, let's click on each image
+            image_element.click()
+            # You can perform any other action you want on each image element.
+            # Add your logic here...
+            opened_img_xpath = """//*[@id="Sva75c"]/div[2]/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]"""
+            max_wait = 0
+            while (
+                driver.find_element(By.XPATH, opened_img_xpath).get_attribute("src").startswith("data:")
+                or driver.find_element(By.XPATH, opened_img_xpath).get_attribute("src").startswith("https://encrypted-tbn0")
+            ):
+                max_wait += 1
+                time.sleep(2)
+                if max_wait == 15:
+                    break
+            opened_img_element = driver.find_element(By.XPATH, opened_img_xpath)
+            image_src_link = opened_img_element.get_attribute('src')
+            print(image_src_link)
+            img_urls.append(image_src_link)
+
+    except Exception as e:
+        print("An error occurred:", e)
+
+    finally:
+        driver.quit()
+
+    # Create a folder to save the downloaded images
+    if not os.path.exists('images'):
+        os.makedirs('images')
+
+    # Check if the urls.txt file exists for the given search keyword
+    urls_file_path = os.path.join('images', f"{search}/urls.txt")
+    if os.path.exists(urls_file_path):
+        # Read the URLs from the file and store them in a list
+        with open(urls_file_path, 'r') as urls_file:
+            urls_open = urls_file.readlines()
+        urls_open = [i.strip() for i in urls_open]
+
+        # Filter out the URLs that are already downloaded
+        img_urls = [url for url in img_urls if url not in urls_open]
+
+    # Loop through the list of image URLs and download each image
+    for idx, url in enumerate(img_urls, start=1):
+        # Generate a unique filename for the image using timestamp and random string
+        timestamp = int(time.time())
+        random_string = generate_random_string(6)
+        filename = f"{timestamp}_{random_string}.jpg"
+
+        save_path = os.path.join(save_folder, filename)
+        download_image(url, save_path)
+
+        # Append the downloaded URL to the urls.txt file
+        with open(urls_file_path, 'a+') as urls_file:
+            urls_file.write(url + '\n')
+
+
+if __name__ == "__main__":
+    main()
